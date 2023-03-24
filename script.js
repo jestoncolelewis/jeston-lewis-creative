61699378664a1792b6fd9278d7684cd8737020bc
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-24 15:29:01 -0600

    updated url and added logs of responses

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function send(n, e, p, s, m) {
-    const url = ''
+    const url = 'https://0cxjlr27zj.execute-api.us-west-2.amazonaws.com/items'
     const usrname = document.getElementById(n)
     const usremail = document.getElementById(e)
     const usrphone = document.getElementById(p)
@@ -26,8 +26,10 @@ function send(n, e, p, s, m) {
     .then((response) => response.json())
     .then((data) => {
         alert('Successfully sent, I\'ll be in touch within the next 1-2 days.');
+        console.log(data);
     })
     .catch((error) => {
         alert('Please try again.');
+        console.log(error);
     })
 }
