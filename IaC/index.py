fe6c6c136491e5c875d0a69a68aec0d67c27e8e8
Jeston Lewis <jeston.lewis@protonmail.com>
2023-03-25 17:59:10 -0600

    fixed getting the proper strings from the JSON

--- IaC/index.py
+++ IaC/index.py
@@ -1,24 +1,25 @@
 import boto3
-import json
 
 ses = boto3.client('sesv2')
 
 def lambda_handler(event, context):
-    replyto = event.get('email')
-    subject = event.get('subject')
-    message = event.get('message')
-    if event.get('routeKey') == 'GET /items':
-        ses.send_email(
-            FromEmailAddress = 'web@jeston.click',
-            FromAddressIdentityArn = 'arn:aws:ses:us-west-2:706391136734:identity/web@jeston.click',
-            Destination = {'ToAddresses': 'jeston@jeston.click'},
-            ReplyToAddresses = replyto,
-            Content = {
-                'Simple': {
-                    'Subject': {'Data': subject},
-                    'Body': {'Text': message}
-                }
+    replyto = event['body']['email']
+    print(replyto)
+    subject = event['body']['subject']
+    print(subject)
+    message = 'Name: ' + event['body']['name'] + '\n' + 'Phone: ' + event['body']['phone'] + '\n' + 'Message: ' + event['body']['message']
+    print(message)
+    response = ses.send_email(
+        FromEmailAddress = 'web@jeston.click',
+        FromEmailAddressIdentityArn = 'arn:aws:ses:us-west-2:706391136734:identity/jeston.click',
+        Destination = {'ToAddresses': ['jeston@jeston.click']},
+        ReplyToAddresses = [replyto],
+        Content = {
+            'Simple': {
+                'Subject': {'Data': subject},
+                'Body': {'Text': {'Data': message}}
             }
-        )
-    return event
+        }
+    )
+    return response
     
